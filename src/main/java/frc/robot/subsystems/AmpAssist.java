e29d836d5d37891a33f5281ca0a025659930269f
Team 4237 Laptop 5 <LakeshoreRoboticsTeam@gmail.com>
2024-01-17 15:41:11 -0500

    RF LB Created Amp Assist

---
+++ src/main/java/frc/robot/subsystems/AmpAssist.java
@@ -0,0 +1,61 @@
+package frc.robot.subsystems;
+
+import java.lang.invoke.MethodHandles;
+
+/**
+ * Use this class as a template to create other subsystems.
+ */
+public class AmpAssist extends Subsystem4237
+{
+    // This string gets the full name of the class, including the package name
+    private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
+
+    // *** STATIC INITIALIZATION BLOCK ***
+    // This block of code is run first when the class is loaded
+    static
+    {
+        System.out.println("Loading: " + fullClassName);
+    }
+    
+    private class PeriodicData
+    {
+        // INPUTS
+
+        // OUTPUTS
+
+    }
+
+    private PeriodicData periodicData = new PeriodicData();
+
+    /** 
+     * Creates a new ExampleSubsystem. 
+     */
+    public AmpAssist()
+    {
+        super("Amp Assist");
+        System.out.println("  Constructor Started:  " + fullClassName);
+
+        
+        System.out.println("  Constructor Finished: " + fullClassName);
+    }
+
+    @Override
+    public void readPeriodicInputs()
+    {}
+
+    @Override
+    public void writePeriodicOutputs()
+    {}
+
+    @Override
+    public void periodic()
+    {
+        // This method will be called once per scheduler run
+    }
+
+    @Override
+    public void simulationPeriodic()
+    {
+        // This method will be called once per scheduler run during simulation
+    }
+}
