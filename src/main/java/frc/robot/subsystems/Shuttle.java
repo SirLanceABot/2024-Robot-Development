81224c1497af33a5890de9608df9220028fab805
Team 4237 Laptop 3 <LakeshoreRoboticsTeam@gmail.com>
2024-01-30 20:28:13 -0500

    Np, TJ - little things in shuttle

--- src/main/java/frc/robot/subsystems/Shuttle.java
+++ src/main/java/frc/robot/subsystems/Shuttle.java
@@ -31,12 +31,18 @@ public class Shuttle extends Subsystem4237
         System.out.println("Loading: " + fullClassName);
     }
 
+    public enum ResetState
+    {
+        kStart, kTry, kDone;
+    }
+
     //makes a new motor, can spark max instantiation
     private final CANSparkMax4237 motor = new CANSparkMax4237(Constants.Shuttle.MOTOR_PORT, Constants.Shuttle.MOTOR_CAN_BUS, "Shuttle Motor");
     // private final CANSparkMax shuttleMotor = new CANSparkMax(Constants.Shuttle.SHUTTLE_MOTOR_PORT, MotorType.kBrushless);
     
     private final double GEAR_RATIO = 3.0 / 20.0; // Roller spins three times, the motor spins 20 times
     private final double ROLLER_CIRCUMFRENCE_INCHES = Math.PI * 2.25; 
+    private final double SHUTTLE_SOFT_LIMIT = 3.00;
 
     private class PeriodicData
     {
@@ -47,6 +53,7 @@ public class Shuttle extends Subsystem4237
         // OUTPUTS
 
     }
+    private ResetState resetState = ResetState.kDone;
 
     private final PeriodicData periodicData = new PeriodicData();
     
@@ -62,7 +69,6 @@ public class Shuttle extends Subsystem4237
 
         motor.setupFactoryDefaults();
 
-        // creates a limit switch for the shuttle motor
         
         configMotor();
 
@@ -75,14 +81,15 @@ public class Shuttle extends Subsystem4237
     private void configMotor()
     {
         motor.setupCoastMode();
-        motor.setupForwardSoftLimit(3.0 * ROLLER_CIRCUMFRENCE_INCHES, true);
-        motor.setupReverseSoftLimit(-3.0 * ROLLER_CIRCUMFRENCE_INCHES, true);
+        motor.setupForwardSoftLimit(SHUTTLE_SOFT_LIMIT * ROLLER_CIRCUMFRENCE_INCHES, true);
+        motor.setupReverseSoftLimit(-SHUTTLE_SOFT_LIMIT * ROLLER_CIRCUMFRENCE_INCHES, true);
 
         // motor.enableSoftLimit(SoftLimitDirection.kForward, true); // kForward is upward movement
         // motor.enableSoftLimit(SoftLimitDirection.kReverse, isEnabled); // kReverse is downward movement
         // motor.enableSoftLimit(SoftLimitDirection.kForward, true);
         //motor.enableSoftLimit(SoftLimitDirection.kForward, true);
-    
+        
+        // creates a limit switch for the shuttle motor
         motor.setupForwardHardLimitSwitch(true, true);
         motor.setupReverseHardLimitSwitch(true, true);
 
@@ -124,7 +131,7 @@ public class Shuttle extends Subsystem4237
      */
     public void resetEncoder()
     {
-        motor.setPosition(0.0);
+        resetState = ResetState.kStart;
     }
 
     public Command moveUpwardCommand()
@@ -147,6 +154,7 @@ public class Shuttle extends Subsystem4237
     public void writePeriodicOutputs()
     {
         motor.set(periodicData.motorSpeed);
+        motor.setPosition(0.0);
     }
 
     @Override
@@ -161,12 +169,12 @@ public class Shuttle extends Subsystem4237
     public void simulationPeriodic()
     {
         // This method will be called once per scheduler run during simulation
+    } 
+
+    @Override
+    public String toString()
+    {
+        return("Encoder = " + motor.getPosition());
     }
-
-    // @Override
-    // private static final void toString()
-    // {
-
-    // }
 
 }
